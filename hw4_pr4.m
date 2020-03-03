cb9047632a33d42daa6777321405113d4f976fdc
Matt Rydalch <matthewk.rydalch@gmail.com>
2020-03-03 09:31:12 -0700

    working on hw 4

--- hw4_pr4.m
+++ hw4_pr4.m
@@ -18,9 +18,31 @@ evalue2 = eig(A2);
 Q = eye(2);
 P = lyap(A2,Q);
 
+k = 0.333333333; %a common coefficient in the equation
+
+x1 = 0;
+x2 = 0;
+v_dot = -1; %just start the loop
+while v_dot <= 0 
+   est_c = k/2*x1^2+k/2*x1*x2+k/2*x2^2;
+   x1 = x1-.001;
+   x2 = x2-.001;
+   v_dot = -k*x1^4+x1*x2-k/2*x1*x2^3-k/2*x1^3*x2-k*x2^4;
+end 
+
 func = @(t,x)[-(x(1)+x(1)^3)+2*x(2); 2*x(1)-(x(2)+x(2)^3)];
-tspan = [0,100];
-x1 = linspace(-15,15,10);
-x2 = linspace(-15,15,10);
-init = [-10 1 4 3.2 7 -3 5; 11 -4.5 5 0 7 -1 1.5];
+tspan = [0,5];
+x1 = linspace(-2,2,10);
+x2 = linspace(-2,2,10);
+init = 0.3*[-10 1 4 3.2 7 -3 5; 11 -4.5 5 0 7 -1 1.5];
 phase_portrait(func, tspan, x1, x2, init)
+
+% x = linspace(-10,10);
+% y = linspace(-10,10);
+% [X1,X2] = meshgrid(x,y);
+% V = c/2*X1^2+c/2*X1*X2+c/2*X2^2;
+% contour(V)
+
+% fun = @(x,c)(-k*x(1)^4+k*x(1)*x(2)-k/2*x(1)*x(2)^3-k/2*x(1)^3*x(2)-k*x(2)^4==0, k/2*X1^2+k/2*X1*X2+k/2*X2^2==c;
+% 
+% x = fmincon(fun)
--- hw4_pr5.m
+++ hw4_pr5.m
@@ -2,10 +2,14 @@
 clear;
 clc;
 
-func = @(t,x)[-x(1)^3-x(2); x(1)-x(2)];
-tspan = [0,5];
-x1 = linspace(-2,2,10);
-x2 = linspace(-2,2,10);
-init = [-1.5 -1 -0.5 0 0.5 1 1.5; -1.5 -1 -0.5 0 0.5 1 1.5];
-
-phase_portrait(func, tspan, x1, x2, init)
+A = [0 -1; 1 -1];
+Q = eye(2);
+P = lyap(A,Q); %Matlab does it backwards AP+P*A' rather than PA+A'P
+%flip signs on off diagnol to fix it.
+% func = @(t,x)[-x(1)^3-x(2); x(1)-x(2)];
+% tspan = [0,5];
+% x1 = linspace(-2,2,10);
+% x2 = linspace(-2,2,10);
+% init = [-1.5 -1 -0.5 0 0.5 1 1.5; -1.5 -1 -0.5 0 0.5 1 1.5];
+% 
+% phase_portrait(func, tspan, x1, x2, init)
