0cb61699b732a2b32ccec5eecc48aca1adfe6db0
Matt Rydalch <matthewk.rydalch@gmail.com>
2020-03-03 18:59:04 -0700

    working on hw 4

--- hw4_pr4.m
+++ hw4_pr4.m
@@ -20,20 +20,56 @@ P = lyap(A2,Q);
 
 k = 0.333333333; %a common coefficient in the equation
 
-x1 = 0;
-x2 = 0;
-v_dot = -1; %just start the loop
-while v_dot <= 0 
-   est_c = k/2*x1^2+k/2*x1*x2+k/2*x2^2;
-   x1 = x1-.001;
-   x2 = x2-.001;
-   v_dot = -k*x1^4+x1*x2-k/2*x1*x2^3-k/2*x1^3*x2-k*x2^4;
-end 
+% x1 = 0;
+% x2 = 0;
+% v_dot = -1; %just start the loop
+% while v_dot <= 0 
+%    est_c = k/2*x1^2+k/2*x1*x2+k/2*x2^2;
+%    x1 = x1-.001;
+%    x2 = x2-.001;
+%    v_dot = -k*x1^4+x1*x2-k/2*x1*x2^3-k/2*x1^3*x2-k*x2^4;
+% end 
+
+scale = 5;
+x1 = linspace(-scale,scale);
+x2 = linspace(-scale,scale);
+[x1,x2] = meshgrid(x1,x2);
+
+% V = k/2*((x1-basis(1)).^2+(x1-basis(1)).*(x2-basis(2))+(x2-basis(2)).^2);
+% M = contour(x1, x2, V);
+% 
+% for i = 1:1:size(M(1))
+%     z1 = x1(1)-basis(1);
+%     z2 = x2(1)-basis(2);
+%     v_dot = -k*z1^4+z1*z2-k/2*z1*z2^3-k/2*z1^3*z2-k*z2^4
+%     if 
+    
+
+basis = [1,1];
+c_cand = [];
+for i = 1:1:size(x1)
+    z1 = x1(i)-basis(1);
+    for j = 1:1:size(x2)
+        z2 = x2(i)-basis(2);
+%         v_dot = -k*z1^4+z1*z2-k/2*z1*z2^3-k/2*z1^3*z2-k*z2^4
+          v_dot = (k*z1+k/2*z2)*(-(z1+z1^3)+2*z2)+(k/2*z1+k*z2)*(2*
+        if v_dot > 0
+            c_new = k/2*(z1^2+z1*z2+z2^2);
+            c_cand = [c_cand, c_new];
+        end
+    end
+end
+
+c = min(c_cand);
+
+V = k/2*((x1-basis(1)).^2+(x1-basis(1)).*(x2-basis(2))+(x2-basis(2)).^2);
+contour(x1, x2, V,c);
+hold on
 
 func = @(t,x)[-(x(1)+x(1)^3)+2*x(2); 2*x(1)-(x(2)+x(2)^3)];
 tspan = [0,5];
-x1 = linspace(-2,2,10);
-x2 = linspace(-2,2,10);
+x1 = linspace(-scale,scale,10);
+x2 = linspace(-scale,scale,10);
 init = 0.3*[-10 1 4 3.2 7 -3 5; 11 -4.5 5 0 7 -1 1.5];
 phase_portrait(func, tspan, x1, x2, init)
 
